b8726198c2412650e9d2660a49731a19124f7a43
christa-baccas <christa.baccas@gmail.com>
2021-11-07 14:24:57 -0600

    removed old files and added new handlebars files

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const dashboardRoutes = require('./');
+const dashboardRoutes = require('./dahsboardRoutes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,3 +1,4 @@
+// a function that uses a post method that directs users to the index of the url when the logout button is clicked
 const logout = async () => {
   const response = await fetch('/api/users/logout', {
     method: 'POST',
